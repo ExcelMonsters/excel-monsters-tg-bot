3a67a413828aea4b4d169cf01d6487ac64c63d2b
Alex <a@b.com>
2016-09-11 01:31:39 +0300

    bot wrapper + our conversations

--- ExcelGuruCore.py
+++ ExcelGuruCore.py
@@ -4,30 +4,45 @@
 # In[1]:
 
 
-
 import pandas as pd
-from pandas import read_excel,read_pickle
-from jellyfish import damerau_levenshtein_distance
-from re import findall
 
 
 # In[2]:
 
-texts = {
-    'start': ['Это бот по мотивам специализированного курса “Excel для интернет - маркетинга”. Он предназначен для интернет - маркетологов, веб-аналитиков, руководителей или владельцев бизнеса желающих научиться использовать все возможности Microsoft Excel for Windows.\n\nПосле начала курса мы попросим тебя установить время для рассылки уведомлений о курсе. И ежедневно мы будем присылать тебе. Новые уроки открываются по расписанию при наличии выполненных домашних заданий от тебя. О невыполненных домашних заданиях мы тебе будем напоминать, но лучше не злоупотребляй - чем быстрее ты выполняешь домашние задания, тем больше очков ты получаешь.\n\nhttps://vimeo.com/181415903\n\nВыбирай мудро.']
-    'start_learning' : ['Отлично! Давай начнем. Для курса нам понадобится Excel 2013 или Excel 2016. В этом коротком видео я расскажу о том, как начать работу с Excel']
-}
+quiz1 = [
+    [
+        ['Каким способом можно добавить кнопки в панель быстрого запуска?\n\nА. перетащив необходимую кнопку в панель быстрого запуска\nБ. Нажав правую кнопку на интересующей кнопке и выбрать пункт отправить на ***\nВ. Позвонить маме']
+        ,['Какое слово съела корова?']
+    ]
+    ,[
+        [['А','Б','В']]
+        ,[['Первое'],['Второе']]
+    ]
+    ,['А','Первое']
+    ]
 
 
 # In[3]:
 
-replies = {
-    'start': [ [['Начать обучение'],['Все уроки']] ]
-    'start_learning' : [ [['Отлично, готов к квизу'],['Вопрос преподавателю'],['Оглавление уроков']] ]
+texts = {
+    'start': ['Привет! Меня зовут ExcelGuru и я сделаю тебя крутейшим чуваком в использовании Excel для интернет-маркетинга. С чего начнём?']
+    ,'learning1' : ['Отлично! Давай начнем. Для курса нам понадобится Excel 2013 или Excel 2016. В этом коротком видео я расскажу о том, как начать работу с Excel']
+    ,'guide': ['Этот бот создан по мотивам специализированного курса “Excel для интернет-маркетинга”. Он предназначен для интернет-маркетологов, веб-аналитиков, руководителей или владельцев бизнеса, желающих научиться использовать все возможности Microsoft Excel\n\nФормат обучения такой:\n• существует рейтинг, в котором в любой момент времени ты можешь сравнить, насколько ты классный по сравнению с другими студентами (которые прошли столько же, сколько и ты)\n• ежедневно мы отправляем тебе небольшое задание, которое можно выполнить буквально на ходу. У тебя 1 день на выполнение за полный балл, дальше - дадим, но просто меньше. Не тормози\n• каждые 5 уроков я буду предлагать тебе добровольные тесты по сразу нескольким пройденным материалам. Так ты сможешь понять, что ты усвоил надолго, а что тебе стоит повторить\n• в любой момент времени, например, когда тебе это понадобится в твоих проектах, ты можешь написать мне вопрос, и я постараюсь тебе автоматически ответить, а если у меня не получится тебе ответить - тогда ты всегда можешь обсудить свои вопросы в нашем коммьюнити: @ExcelGuruCommunity или задать вопрос преподавателю: @maxim_uvarov\n\nhttps://vimeo.com/181415903\nhttp://needfordata.ru/excel\n\nНачнём?']
+    ,'overview': [''] # внутри функции
 }
 
 
 # In[4]:
+
+replies = {
+    'start': [ [['Прочитать гайд'],['Начать обучение'],['Обзор уроков']] ]
+    ,'learning1' : [ [['Отлично, готов к quiz'],['Обзор уроков']] ]
+    ,'guide' : [ [['Начать обучение'],['Обзор уроков']] ]
+    ,'overview': [ [[]] ] # внутри функции
+}
+
+
+# In[5]:
 
 class ExcelGuruCore:
 
@@ -35,9 +50,12 @@ class ExcelGuruCore:
         self.file_user_data = 'user_data/user_data.pkl'
         self.texts = texts
         self.replies = replies
+        self.ku = 'Ничего не найдено :('
+        self.quizes = [quiz1]
         
     # внутренние функции  
-        
+    
+    def unlist(self,l): return([item for sublist in l for item in sublist])
         
     def get_state(self,chat_id):
         user_data = pd.read_pickle(self.file_user_data)
@@ -69,66 +87,60 @@ class ExcelGuruCore:
     
     def process_txt(self,chat_id,txt=''):
         if self.get_state(chat_id) == '': return(self.slash_start(chat_id))
-        elif self.get_state(chat_id) == 'switch_guide_or_overview': return(self.switch_guide_or_overview(chat_id,txt))
+        elif self.get_state(chat_id) == 'switch_start': return(self.switch_start(chat_id,txt))
+        elif self.get_state(chat_id) == 'switch_guide': return(self.switch_guide(chat_id,txt))
+        elif self.get_state(chat_id) == 'switch_learning1': return(self.switch_learning1(chat_id,txt))
         else: return(-1,-1)
     
-    # состояния и команды
+    # команды
     
     def slash_start(self,chat_id,txt = ''):
-        messages = texts['start']
-        reply_keyboard = replies['start']
-        self.set_state(chat_id,'switch_guide_or_overview')
-        
-        return(messages,reply_keyboard)
-    
-    def switch_guide_or_overview(self,chat_id,txt):
-        if txt == 'Все уроки': #or smth else
-            return(self.state_start_guide(chat_id,txt))
-        elif txt == 'Начать обучение':
-            return(self.state_overview(chat_id,txt))
-        else:
-            return(['Ничего не понял :('],[[[]]])
+        self.set_state(chat_id,'switch_start')
+        return(texts['start'],replies['start'])
             
-    
-    def state_start_guide(self,chat_id,txt):
-        state = ''
-        messages = []
-        reply_keyboard = []
-        
-        if txt == 'Расскажи': # другие варианты ответа
-            msg = 'Ае! Обожаю рассказывать. Если надоест - нажми /skip, и мы начнём заниматься.'
-            messages.append()
-            reply_keyboard.append([[[]]])
-            state = 'guide0'
-        elif txt == 'Давай начнём': # другие варианты ответа
-            messages.append('Хорошо, но если ты захочешь всё же ознакомиться - нажми /start. А сейчас я расскажу о плане курса.')
-            reply_keyboard.append([[[]]])
-            state = 'overview'
-        else: # непонятненько
-            messages.append('К сожалению, я ничего не понял :( Пожалуйста, используй кнопки, я их сделал, чтобы ты мог быстрее со мной общаться')
-            reply_keyboard.append([[[]]])
-        
-        return(state,messages,reply_keyboard)
+    # состояния
+
+    def state_guide(self,chat_id,txt):
+        self.set_state(chat_id,'switch_guide')
+        return(texts['guide'],replies['guide'])
     
     def state_overview(self,chat_id,txt):
-        messages = []
-        reply_keyboard = []
-        state = ''
-        
-        messages.append(txt_overview)
-        reply_keyboard.append([[['Приступить к обучению'],['Более подробная информация о уроках']]])        
-        state = 'learning_or_more_info'
-        
-        return(state,messages,reply_keyboard)
+        self.set_state(chat_id,'switch_overview')
+        return(texts['overview'],replies['overview'])
     
-    def state_learning_or_more_info(self,chat_id,txt):
-        messages = []
-        reply_keyboard = []
-        state = ''
-        
-        messages.append(txt_overview)
-        reply_keyboard.append([[['Приступить к обучению'],['Более подробная информация о уроках']]])        
-        state = 'start_learning_or_more_info'
-        
-        return(state,messages,reply_keyboard)
+    def state_learning1(self,chat_id,txt):
+        self.set_state(chat_id,'switch_learning1')
+        return(texts['learning1'],replies['learning1'])
+    
+    def state_quiz1(self,chat_id,txt):
+        self.set_state(chat_id,'switch_quiz1')
+        return(texts['quiz1'],replies['quiz1'])
+
+    # разветвления после разных состояний (1 состояние = 1 разветвление)
+    
+    def switch_start(self,chat_id,txt):
+        if txt == self.unlist(replies['start'][0])[0]: #guide
+            return(self.state_guide(chat_id,txt))
+        elif txt == self.unlist(replies['start'][0])[1]: #learning
+            return(self.state_learning1(chat_id,txt))
+        elif txt == self.unlist(replies['start'][0])[2]: #overview
+            return(self.state_overview(chat_id,txt))
+        else:
+            return([self.ku],replies['start'])
+
+    def switch_guide(self,chat_id,txt):
+        if txt == self.unlist(replies['guide'][0])[0]: #learning
+            return(self.state_learning1(chat_id,txt))
+        elif txt == self.unlist(replies['guide'][0])[1]: #overview
+            return(self.state_overview(chat_id,txt))
+        else:
+            return([self.ku],replies['guide'])
+
+    def switch_learning1(self,chat_id,txt):
+        if txt == self.unlist(replies['learning1'][0])[0]: #quiz
+            return(self.state_quiz1(chat_id,txt))
+        elif txt == self.unlist(replies['learning1'][0])[1]: #overview
+            return(self.state_overview(chat_id,txt))
+        else:
+            return([self.ku],replies['learning1'])
 
---
+++ ExcelGuruWrapper.py
@@ -0,0 +1,144 @@
+
+# coding: utf-8
+
+# ### Библиотечки
+
+# In[ ]:
+
+from sys import exc_info# для выдачи ошибок
+from time import strftime # для логгирования
+from os import environ # для TG_TOKEN
+
+
+# In[ ]:
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler,RegexHandler # для работы бота в целом
+from telegram import ReplyKeyboardMarkup,ReplyKeyboardHide,ReplyMarkup # для инлайновой клавиатуры
+
+
+# ### Грузим класс, выполняющий ядро бота
+
+# In[ ]:
+
+from ExcelGuruCore import *
+
+
+# In[ ]:
+
+eg = ExcelGuruCore()
+
+
+# ### Создаём дополнительные функции
+
+# Функция, которая разбивает превашающие лимит размера сообщения телеграма на блоки
+
+# In[ ]:
+
+def split_msg(txt):
+    if len(txt)<4095: return([txt])
+    
+    l = txt.split('\n')
+    for p in l:
+        if len(p)>4095:
+            return(-1) #impossible then
+        #may go into each p > 4095, change in loop there every last " " to "\n"
+        #for poems may split firstly by "\n\n", only then inside by "\n", "."," "
+    
+    s = 0
+    for i in range(1,len(l)):
+        t = "\n".join(l[:i])
+        if len(t)>4095:
+            s = i
+            break
+    s = s-1        
+    
+    resp = "\n".join(l[:s])
+    left = "\n".join(l[s:])
+    if len(left)<4096: return([resp,left])
+    else: return([rest]+split_msg(left))
+
+
+# ### Именно телеграм
+
+# In[ ]:
+
+def slash_help(bot, update):
+    inside_idle(bot,update,'help')
+
+
+# In[ ]:
+
+def slash_start(bot, update):
+    txt = update.message.text
+    chat_id = update.message.chat_id
+    msgs,buttons = eg.slash_start(chat_id)
+    print(buttons)
+    for i in range(len(msgs)):
+        print(buttons[i])
+        bot.sendMessage(chat_id=chat_id, text = msgs[i],reply_markup=ReplyKeyboardMarkup(buttons[i], one_time_keyboard=True))
+
+
+# In[ ]:
+
+def inside_idle(bot,update,txt=-1,chat_id=-1):
+    if txt == -1:
+        txt = update.message.text
+    if chat_id == -1:
+        chat_id = update.message.chat_id
+    try:
+        msgs,buttons = eg.process_txt(chat_id,txt)
+        print('### got: '+txt)
+        print('state: ' + eg.get_state(chat_id))
+        print(msgs)
+        print(buttons)
+        if msgs == -1:
+            bot.sendMessage(chat_id=chat_id, text = "smth wrong with states")
+            return(0)
+        for i in range(len(msgs)):
+            bot.sendMessage(chat_id=chat_id, text = msgs[i],reply_markup=ReplyKeyboardMarkup(buttons[i], one_time_keyboard=True))
+    except:
+        bot.sendMessage(chat_id=chat_id, text = "Unexpected error:" + str(sys.exc_info()[0]))
+
+
+# In[ ]:
+
+def idle_main(bot, update):
+    inside_idle(bot,update)
+
+
+# In[ ]:
+tg_token = environ['TG_TOKEN']
+
+def main():
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(tg_token)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+    
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", slash_start),group=0)
+    dp.add_handler(CommandHandler("help", slash_help),group=0)
+    
+    # on noncommand message
+    dp.add_handler(MessageHandler([Filters.text], idle_main))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot
+    updater.idle()
+
+
+# ### Запускаем!
+
+# In[ ]:
+
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
+
+
+
